e07e75f27e672503a8dc72702930bebeb751d968
Soulé Soumaré <sourtoumo@gmail.com>
2025-05-08 18:35:56 +0200

    First commit

--- src/main/java/com/learn/kafka/repository/ExchangeRateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.learn.kafka.repository;
-
-import com.learn.kafka.model.ExchangeRate;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ExchangeRateRepository extends MongoRepository<ExchangeRate, String> {
-}
