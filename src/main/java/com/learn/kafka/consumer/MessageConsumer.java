d56134726a3928628693e87d69c8f9fa2d8266e2
Soulé Soumaré <sourtoumo@gmail.com>
2025-05-08 17:30:22 +0200

    step 1 config and tests

--- src/main/java/com/learn/kafka/consumer/MessageConsumer.java
+++ src/main/java/com/learn/kafka/consumer/MessageConsumer.java
@@ -1,13 +1,16 @@
 package com.learn.kafka.consumer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class MessageConsumer {
 
-    @KafkaListener(topics = "${spring.kafka.template.default-topic}", groupId = "${spring.kafka.consumer.group-id}")
-    public void consume(String message) {
-        System.out.println("Message consommé : " + message);
+    @KafkaListener(topics = "${spring.kafka.template.default-topic}", 
+            groupId = "${spring.kafka.consumer.group-id}")
+    public void listen(String message) {
+        log.info("Message received: {}", message);
     }
 }
