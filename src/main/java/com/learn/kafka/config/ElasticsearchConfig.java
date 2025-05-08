7cd2e11f14402d1fd4463d9cc19ffcf2b6b5a2ce
Soulé Soumaré <sourtoumo@gmail.com>
2025-05-08 20:18:17 +0200

    refactor code

--- src/main/java/com/learn/kafka/config/ElasticsearchConfig.java
+++ src/main/java/com/learn/kafka/config/ElasticsearchConfig.java
@@ -3,21 +3,10 @@ package com.learn.kafka.config;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> c550199 (Exchange API, realtime time application)
 import org.apache.http.HttpResponseInterceptor;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
-<<<<<<< HEAD
-=======
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
->>>>>>> c6756da (Upgrade packages to latest versions)
-=======
->>>>>>> c550199 (Exchange API, realtime time application)
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -26,17 +15,12 @@ import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 @Configuration
 public class ElasticsearchConfig {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> c550199 (Exchange API, realtime time application)
     @Value("${elasticsearch.host}")
     private String elasticsearchHost;
 
     @Value("${elasticsearch.port}")
     private int elasticsearchPort;
 
-<<<<<<< HEAD
     @Bean
     ElasticsearchClient elasticsearchClient() {
         // Configurer RestClient avec un en-tête compatible Elasticsearch
@@ -57,46 +41,11 @@ public class ElasticsearchConfig {
         RestClient restClient = builder.build();
 
         // Configurer le transport
-=======
-=======
->>>>>>> c550199 (Exchange API, realtime time application)
-    @Bean
-    ElasticsearchClient elasticsearchClient() {
-        // Configurer RestClient avec un en-tête compatible Elasticsearch
-        RestClientBuilder builder = RestClient.builder(new HttpHost(elasticsearchHost, elasticsearchPort));
-        builder.setDefaultHeaders(new org.apache.http.Header[]{
-                new org.apache.http.message.BasicHeader("Content-Type", "application/json")
-        });
-
-        // Désactiver la vérification stricte des en-têtes
-        builder.setStrictDeprecationMode(false);
-
-        // Ajouter un intercepteur pour inclure l'en-tête X-Elastic-Product dans la réponse
-        builder.setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.addInterceptorLast(
-                (HttpResponseInterceptor) (response, _) -> 
-                        response.addHeader("X-Elastic-Product", "Elasticsearch")
-        ));
-
-        RestClient restClient = builder.build();
-
-<<<<<<< HEAD
-        // Configurer le transport avec JacksonJsonpMapper
->>>>>>> c6756da (Upgrade packages to latest versions)
-=======
-        // Configurer le transport
->>>>>>> c550199 (Exchange API, realtime time application)
         RestClientTransport transport = new RestClientTransport(
                 restClient,
                 new JacksonJsonpMapper(new ObjectMapper())
         );
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-        // Retourner le client Elasticsearch
->>>>>>> c6756da (Upgrade packages to latest versions)
-=======
->>>>>>> c550199 (Exchange API, realtime time application)
         return new ElasticsearchClient(transport);
     }
 }
--- src/main/java/com/learn/kafka/repository/ElasticsearchRepository.java
+++ src/main/java/com/learn/kafka/repository/ElasticsearchRepository.java
@@ -1,11 +1,6 @@
 package com.learn.kafka.repository;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 0729c7d (refactor code)
 import co.elastic.clients.elasticsearch.indices.CreateIndexRequest;
 import co.elastic.clients.elasticsearch.indices.CreateIndexResponse;
 import co.elastic.clients.elasticsearch.core.BulkRequest;
@@ -14,31 +9,11 @@ import co.elastic.clients.elasticsearch.core.bulk.BulkOperation;
 import com.learn.kafka.model.ExchangeRate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-<<<<<<< HEAD
-=======
-import co.elastic.clients.elasticsearch.core.IndexRequest;
-=======
-import co.elastic.clients.elasticsearch.core.BulkRequest;
-import co.elastic.clients.elasticsearch.core.BulkResponse;
-import co.elastic.clients.elasticsearch.core.bulk.BulkOperation;
->>>>>>> c550199 (Exchange API, realtime time application)
-import com.learn.kafka.model.ExchangeRate;
-import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> c6756da (Upgrade packages to latest versions)
-=======
->>>>>>> 0729c7d (refactor code)
 import org.springframework.stereotype.Repository;
 
 import java.io.IOException;
 import java.util.List;
-<<<<<<< HEAD
-<<<<<<< HEAD
 import java.util.stream.Collectors;
-=======
->>>>>>> c6756da (Upgrade packages to latest versions)
-=======
-import java.util.stream.Collectors;
->>>>>>> c550199 (Exchange API, realtime time application)
 
 @Repository
 public class ElasticsearchRepository {
@@ -46,8 +21,6 @@ public class ElasticsearchRepository {
     @Autowired
     private ElasticsearchClient elasticsearchClient;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     @Value("${elasticsearch.index.name}")
     private String indexName;
 
@@ -82,66 +55,6 @@ public class ElasticsearchRepository {
         } catch (IOException e) {
             throw new RuntimeException("Erreur lors de l'indexation en Bulk dans Elasticsearch : " + e.getMessage());
         }
-    }
-
-    private void createIndexWithMapping() throws IOException {
-        CreateIndexRequest request = new CreateIndexRequest.Builder()
-                .index(indexName)
-                .mappings(m -> m
-                        .properties("timestamp", p -> p.date(d -> d))
-                        .properties("base", p -> p.keyword(k -> k))
-                        .properties("currency", p -> p.keyword(k -> k))
-                        .properties("rate", p -> p.double_(d -> d))
-                )
-                .build();
-
-        CreateIndexResponse response = elasticsearchClient.indices().create(request);
-
-        if (!response.acknowledged()) {
-            throw new RuntimeException("La création de l'index Elasticsearch a échoué.");
-        }
-=======
-=======
-    @Value("${elasticsearch.index.name}")
-    private String indexName;
-
->>>>>>> 0729c7d (refactor code)
-    public void indexAll(List<ExchangeRate> rates) {
-        try {
-            // Vérifier si l'index existe, sinon le créer avec un mapping explicite
-            if (!elasticsearchClient.indices().exists(e -> e.index(indexName)).value()) {
-                createIndexWithMapping();
-            }
-
-            // Préparer les opérations Bulk
-            List<BulkOperation> operations = rates.stream()
-                    .map(rate -> BulkOperation.of(op -> op
-                            .index(idx -> idx
-                                    .index(indexName)
-                                    .document(rate)
-                            )
-                    ))
-                    .collect(Collectors.toList());
-
-            // Construire et exécuter la requête Bulk
-            BulkRequest bulkRequest = new BulkRequest.Builder()
-                    .operations(operations)
-                    .build();
-
-            BulkResponse response = elasticsearchClient.bulk(bulkRequest);
-
-            // Vérifier les erreurs dans la réponse
-            if (response.errors()) {
-                throw new RuntimeException("Certaines opérations Bulk ont échoué : " + response.items());
-            }
-<<<<<<< HEAD
-        });
->>>>>>> c6756da (Upgrade packages to latest versions)
-=======
-        } catch (IOException e) {
-            throw new RuntimeException("Erreur lors de l'indexation en Bulk dans Elasticsearch : " + e.getMessage());
-        }
->>>>>>> c550199 (Exchange API, realtime time application)
     }
 
     private void createIndexWithMapping() throws IOException {
